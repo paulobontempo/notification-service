ad21676282cbd2e40a99f325e4c89fefaf1d8f56
Paulo Bontempo <paulobontempo@gmail.com>
2022-12-16 06:26:10 -0300

    feat: send notification

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma.service';
+import { randomUUID } from 'node:crypto';
 
-@Controller()
+@Controller('notifications')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly prismaService: PrismaService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async list() {
+    return this.prismaService.notification.findMany();
+  }
+
+  @Post()
+  async create(@Body() body: any) {
+    const { recipientId, content, category } = body;
+    return this.prismaService.notification.create({
+      data: {
+        id: randomUUID(),
+        content,
+        category,
+        recipientId,
+      },
+    });
   }
 }

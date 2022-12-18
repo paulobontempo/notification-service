9dc22a05fa20611bbaac1599e31d902fb7f7eba6
Paulo Bontempo <paulobontempo@gmail.com>
2022-12-18 09:29:33 -0300

    chore: software redesign strtucture

---
+++ test/repositories/in-memory-notifications-repository.ts
@@ -0,0 +1,11 @@
+import { Notification } from 'src/application/entities/notification';
+import { NotificationsRepository } from 'src/application/repositories/notifications-repository';
+
+export class InMemoryNotificationRepository implements NotificationsRepository {
+  public notifications: Notification[] = [];
+
+  async create(notification: Notification) {
+    console.log(notification);
+    this.notifications.push(notification);
+  }
+}

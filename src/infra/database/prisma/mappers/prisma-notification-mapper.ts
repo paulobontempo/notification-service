1bba6306ca1f9e4c49adb9e26fadfceea1ed389d
Paulo Bontempo <paulobontempo@gmail.com>
2022-12-19 19:27:04 -0300

    feat: add endpoints cancel, count and reads

--- src/infra/database/prisma/mappers/prisma-notification-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -1,15 +1,17 @@
-import { Content } from "@application/entities/content";
-import { Notification } from "@application/entities/notification";
+import { Content } from '@application/entities/content';
+import { Notification } from '@application/entities/notification';
 import { Notification as RawNotification } from '@prisma/client';
 
 export class PrismaNotificationMapper {
-  static toPrisma(notification: Notification){
-    return {id: notification.id,
-      content: notification.content.value,
+  static toPrisma(notification: Notification) {
+    return {
+      id: notification.id,
       category: notification.category,
+      content: notification.content.value,
       recipientId: notification.recipientId,
       readAt: notification.readAt,
-      createdAt: notification.createdAt,}
+      createdAt: notification.createdAt,
+    };
   }
   static toDomain(raw: RawNotification): Notification {
     return new Notification(
--- src/infra/http/view-models/notification-view-model.ts
+++ src/infra/http/view-models/notification-view-model.ts
@@ -1,12 +1,14 @@
-import { Notification } from "../../../application/entities/notification";
+import { Notification } from '@application/entities/notification';
 
 export class NotificationViewModel {
   static toHTTP(notification: Notification) {
     return {
       id: notification.id,
-      content: notification.content.value,
       category: notification.category,
+      content: notification.content.value,
       recipientId: notification.recipientId,
-    }
+      readAt: notification.readAt,
+      createdAt: notification.createdAt,
+    };
   }
 }
